41d04298ddb79d4d2299ba3aa84b679a4ab15bc6
DOHand <2524864581@qq.com>
2026-06-10 14:17:13 +0800

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dohand-tarot/',
   build: {
     outDir: 'docs',
     rollupOptions: {
